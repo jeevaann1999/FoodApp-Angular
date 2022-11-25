847be060cb7b6eb7688ff2118a40df43a40b3ac5
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-25 07:52:41 +0530

    south indian added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { NorthIndianComponent } from './north-indian/north-indian.component';
+import { SouthIndianComponent } from './south-indian/south-indian.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignUpComponent,
-    NorthIndianComponent
+    NorthIndianComponent,
+    SouthIndianComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/south-indian/south-indian.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-south-indian',
+  templateUrl: './south-indian.component.html',
+  styleUrls: ['./south-indian.component.css']
+})
+export class SouthIndianComponent {
+
+}
