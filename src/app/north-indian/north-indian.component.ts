4655adf2bea47875dc8e25b970ecc14254cdcdc8
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-25 07:45:44 +0530

    added north indian

---
+++ src/app/north-indian/north-indian.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-north-indian',
+  templateUrl: './north-indian.component.html',
+  styleUrls: ['./north-indian.component.css']
+})
+export class NorthIndianComponent {
+
+}
