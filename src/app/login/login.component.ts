1e2ead93f5d45adf3ec7c4022e74847950eeba0a
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-29 06:17:17 +0530

    read values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+username=""
+password=""
 
+readValues=()=>{
+  let data:any={
+    "username":this.username,
+    "password":this.password
+  }
+  console.log(data)
+
+
+  if (this.username=="food" && this.password=="1234") {
+    alert("Valid credentials")
+  } else {
+    alert("Invalid credentials")
+  }
+}
 }
